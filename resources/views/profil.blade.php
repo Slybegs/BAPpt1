5dc0362bdbf1526429a2101b8cf0fb6a5483eace
Sully Tania <sullytania@icloud.com>
2023-09-17 23:55:03 +0700

    BAPpt1

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>
+        th {
+            border: 1px solid;
+            text-align: center;
+            padding: 12px;
+        }
+        td {
+            border: 1px solid;
+            text-align: center;
+            padding: 12px;
+        }
+    </style>
+</head>
+<body>
+    <div class= "container">
+        <p style= "font-size: 48px ; font-weight: bold ; text-align: start ; margin: 40px 0 0 0 ; text-decoration: underline">BIODATA</p>
+        <div class= "row">
+            <div class= "col-3">
+                Nama <a style= "margin-left: 200px">:</a>
+            </div>
+            <div class= "col-9">
+                Sully Tania
+            </div>
+        </div>   
+
+        <div class= "row">
+            <div class= "col-3">
+                Student ID <a style= "margin-left: 166px">:</a>
+            </div>
+            <div class= "col-9">
+                03081210016
+            </div>
+        </div>  
+
+        <div class= "row">
+            <div class= "col-3">
+                Kelas <a style= "margin-left: 204px">:</a>
+            </div>
+            <div class= "col-9">
+                21SI2
+            </div>
+        </div>  
+
+        <div class= "row">
+            <div class= "col-3">
+                Fakultas <a style= "margin-left: 183px">:</a>
+            </div>
+            <div class= "col-9">
+                Ilmu Komputer
+            </div>
+        </div>  
+
+        <div class= "row">
+            <div class= "col-3">
+                Program Studi <a style= "margin-left: 140px">:</a>
+            </div>
+            <div class= "col-9">
+                Sistem Informasi
+            </div>
+        </div>  
+        
+        <table style= "margin-top: 16px">
+            <tr>
+                <th>
+                    Matakuliah
+                </th>
+                <th>
+                    SKS
+                </th>
+                <th>
+                    Dosen Pengampu
+                </th>
+            </tr>
+
+            <tr>
+                <td>
+                    Dasar Manajemen Keuangan
+                </td>
+                <td>
+                    3
+                </td>
+                <td>
+                    Lisa Salim
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    Pemrograman Aplikasi Bisnis
+                </td>
+                <td>
+                    4
+                </td>
+                <td>
+                    Ade Maulana
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    Pemasaran Digital 
+                </td>
+                <td>
+                    3
+                </td>
+                <td>
+                    Okky P. Barus
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Audit & Kontrol Sistem Informasi
+                </td>
+                <td>
+                    4
+                </td>
+                <td>
+                    Ade Maulana
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Manajemen Stategis Perusahaan
+                </td>
+                <td>
+                    3
+                </td>
+                <td>
+                    Romindo
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Sistem Informasi Perusahaan
+                </td>
+                <td>
+                    3
+                </td>
+                <td>
+                    Ade Maulana
+                </td>
+            </tr>
+        </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('/Welcome', function () {
     return view('welcome');
 });
+Route::get('/Profil', function () {
+    return view('profil');
+});
